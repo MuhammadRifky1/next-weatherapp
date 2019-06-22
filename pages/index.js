69bda68ccbce5a5718076a9edc9fc1b0f4325b74
MuhammadRifky1 <rifky.rangkuti@gmail.com>
2019-06-22 23:54:12 +0700

    API CALL

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>;
-  }
+import fetch from 'isomorphic-unfetch'
+//import Form from './components/Forms'
+//import Weather from './components/Weather'
+
+const API_KEY = 'a3536c23419eaaa0556cecaae8ae73bb';
+
+function Home({ weather }) {
+  console.table({weather})
+  return <div>{weather.sys.country}</div>
+}
+
+Home.getInitialProps = async ({ req }) => {
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=bogor,id&appid=${API_KEY}`);
+  const json = await res.json();
+  return { weather: json };
+};
   
-  export default Home;
+export default Home;
